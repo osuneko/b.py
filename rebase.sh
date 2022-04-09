8ebf74ca730ec85a982ee41024138843edce7291
minisbett <no>
2022-04-09 11:43:51 +0200

    Added rebase script

---
+++ rebase.sh
@@ -0,0 +1,7 @@
+git remote rm origin
+git remote add origin https://github.com/osuakatsuki/bancho.py
+
+git pull --rebase
+
+git remote rm origin
+git remote add origin https://github.com/cs0su/bancho.py
