3020ac47d926004bac2eb305f7eedbfce12c2be7
minisbett <no>
2022-05-02 11:56:53 +0200

    rebase and uh change stuff and uh rebase2 yeah never again

--- rebase.sh
+++ rebase.sh
@@ -2,6 +2,3 @@ git remote rm origin
 git remote add origin https://github.com/osuakatsuki/bancho.py
 
 git pull origin master --rebase
-
-git remote rm origin
-git remote add origin https://github.com/cs0su/bancho.py
